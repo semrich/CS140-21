3454e731fa34bb311f8d80fbfbb2ac0d1a3c1d90
Scott Joseph Emrich <semrich@tesla1.eecs.utk.edu>
2021-03-14 09:07:47 -0400

    adding lab 8 files

---
+++ labs/lab8/src/hash_140.cpp
@@ -0,0 +1,32 @@
+#include "hash_140.hpp"
+using namespace std;
+
+string Hash_140::Set_Up(size_t table_size, const std::string &fxn, const std::string &collision)
+{
+  (void) table_size;
+  (void) fxn;
+  (void) collision;
+  return "";
+}
+
+string Hash_140::Add(const string &key, const string &val)
+{
+  (void) key;
+  (void) val;
+  return "";
+}
+
+string Hash_140::Find(const string &key)
+{
+  (void) key;
+  return "";
+}
+
+void Hash_140::Print() const
+{
+}
+
+size_t Hash_140::Total_Probes()
+{
+  return 0;
+}
